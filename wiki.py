f4ae45ad971c1af9ac1b0b4814a8f8c16cc2c11e
Naveen S <naveeeen3000@gmail.com>
2022-01-31 10:01:46 +0530

    added some documentation

--- wiki.py
+++ wiki.py
@@ -1,12 +1,20 @@
+## Imports
 from re import search
 from urllib import response
 import requests
+
+## setting the URL 
 search_key=input("Enter search key\n")
 search_key=search_key.replace(" ","_")
 URL="https://en.wikipedia.org/wiki/Special:Search?search="+search_key
+
+
+## Gettings response url 
 response=requests.request("GET",URL)
-file=open("log.txt","a")
 response_url=(response.url)
+
+## writing to a log file
+file=open("log.txt","a")
 print(response_url)
 file.write(response_url+"\n")
 file.close()
--- wiki.sh
+++ wiki.sh
@@ -19,9 +19,12 @@ done
 
 s_key=$1;
 search_key="${s_key// /_}";
-#search_key="${s_key// /_}";
-#echo $search_key;
+
+
+##### Concatenating the search key with the url
 search_url="https://en.wikipedia.org/wiki/Special:Search?search="$search_key;
+
+##### Making a request to wikipedia and capturing the response url and wirting the same to ouput stream and log file using tee
 curl -L -# -o /dev/null --write-out %{url_effective} $search_url| tee -a log.txt;
 echo "" | cat >> log.txt;
 echo "";
